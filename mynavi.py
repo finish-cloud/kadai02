582563a6968d0fe58a09a629bde7b75562110b1a
masaya <hope.is.a.waking.dream.8.1@gmail.com>
2021-06-04 00:02:33 +0900

    課題2-7終了

--- mynavi.py
+++ mynavi.py
@@ -2,7 +2,14 @@ import os
 from selenium.webdriver import Chrome, ChromeOptions
 from selenium.webdriver.chrome.webdriver import WebDriver
 import time
+import datetime
 import pandas as pd
+
+
+LOG_FILE_PATH = "./log/log_{datetime}.log"
+EXP_CSV_PATH = "./exp_list_{search_keyword}_{datetime}.csv"
+log_file_path = LOG_FILE_PATH.format(
+    datetime=datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S'))
 
 # Chromeを起動する関数
 
@@ -26,7 +33,15 @@ def set_driver(driver_path, headless_flg):
     # ChromeのWebDriverオブジェクトを作成する。
     return Chrome(executable_path=os.getcwd() + "/" + driver_path, options=options)
 
-# main処理
+
+# ログファイルおよびコンソール出力
+def log(txt):
+    now = datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
+    logStr = '[%s: %s] %s' % ('log', now, txt)
+    # ログ出力
+    with open(log_file_path, 'a', encoding='utf-8_sig') as f:
+        f.write(logStr + '\n')
+    print(logStr)
 
 
 def find_table_target_word(th_elms, td_elms, target: str):
@@ -35,22 +50,22 @@ def find_table_target_word(th_elms, td_elms, target: str):
         if th_elm.text == target:
             return td_elm.text
 
+# main処理
+
 
 def main():
+    log("処理開始")
     search_keyword = input("検索キーワードを入力してください：")
+    log("検索キーワード:{}".format(search_keyword))
     # driverを起動
-    if os.name == 'nt':  # Windows
-        driver = set_driver("chromedriver.exe", False)
-    elif os.name == 'posix':  # Mac
-        driver = set_driver("chromedriver", False)
+    driver = set_driver("chromedriver.exe", False)
     # Webサイトを開く
     driver.get("https://tenshoku.mynavi.jp/")
-    time.sleep(10)
-
+    time.sleep(5)
     try:
-        # ポップアップを閉じる
+        # ポップアップを閉じる（seleniumだけではクローズできない）
         driver.execute_script('document.querySelector(".karte-close").click()')
-        time.sleep(10)
+        time.sleep(5)
         # ポップアップを閉じる
         driver.execute_script('document.querySelector(".karte-close").click()')
     except:
@@ -63,12 +78,15 @@ def main():
     driver.find_element_by_class_name("topSearch__button").click()
 
     # ページ終了まで繰り返し取得
+    exp_name_list = []
+    exp_copy_list = []
+    exp_status_list = []
+    exp_first_year_fee_list = []
+    count = 0
+    success = 0
+    fail = 0
     while True:
-        exp_name_list = []
-        exp_copy_list = []
-        exp_status_list = []
-        exp_first_year_fee_list = []
-        # 検索結果の一番上の会社名を取得
+        # 検索結果の一番上の会社名を取得(まれに１行目が広告の場合、おかしな動作をするためcassetteRecruit__headingで広告を除外している)
         name_list = driver.find_elements_by_css_selector(
             ".cassetteRecruit__heading .cassetteRecruit__name")
         copy_list = driver.find_elements_by_css_selector(
@@ -77,10 +95,11 @@ def main():
             ".cassetteRecruit__heading .labelEmploymentStatus")
         table_list = driver.find_elements_by_css_selector(
             ".cassetteRecruit .tableCondition")  # 初年度年収
-        # 1ページ分繰り返し
 
+        # 1ページ分繰り返し
         for name, copy, status, table in zip(name_list, copy_list, status_list, table_list):
             try:
+                # try~exceptはエラーの可能性が高い箇所に配置
                 exp_name_list.append(name.text)
                 exp_copy_list.append(copy.text)
                 exp_status_list.append(status.text)
@@ -88,27 +107,34 @@ def main():
                 first_year_fee = find_table_target_word(table.find_elements_by_tag_name(
                     "th"), table.find_elements_by_tag_name("td"), "初年度年収")
                 exp_first_year_fee_list.append(first_year_fee)
+                log(f"{count}件目成功 : {name.text}")
+                success += 1
             except Exception as e:
-                print(e)
+                log(f"{count}件目失敗 : {name.text}")
+                log(e)
+                fail += 1
+            finally:
+                # finallyは成功でもエラーでも必ず実行
+                count += 1
 
-                # 次のページボタンがあればクリックなければ終了
-        next_page = driver.find_elements_by_class_name(
-            "iconFont--arrowLeft")
+        # 次のページボタンがあればクリックなければ終了
+        next_page = driver.find_elements_by_class_name("iconFont--arrowLeft")
         if len(next_page) >= 1:
             next_page_link = next_page[0].get_attribute("href")
             driver.get(next_page_link)
         else:
-            print("最終ページです。終了します。")
+            log("最終ページです。終了します。")
             break
 
-        # CSV出力
-        df = pd.DataFrame({"企業名": exp_name_list,
-                           "キャッチコピー": exp_copy_list,
-                           "ステータス": exp_status_list,
-                           "初年度年収": exp_first_year_fee_list})
-        df.to_csv('求人情報.csv', index=False, encoding="utf-8-sig")
-        driver.quit()
-        return False
+    # CSV出力
+    now = datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
+    df = pd.DataFrame({"企業名": exp_name_list,
+                       "キャッチコピー": exp_copy_list,
+                       "ステータス": exp_status_list,
+                       "初年度年収": exp_first_year_fee_list})
+    df.to_csv(EXP_CSV_PATH.format(search_keyword=search_keyword,
+              datetime=now), encoding="utf-8-sig")
+    log(f"処理完了 成功件数: {success} 件 / 失敗件数: {fail} 件")
 
 
 # 直接起動された場合はmain()を起動(モジュールとして呼び出された場合は起動しないようにするため)
